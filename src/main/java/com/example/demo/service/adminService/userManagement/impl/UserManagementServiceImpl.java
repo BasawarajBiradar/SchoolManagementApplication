38cd867947f83ef22b424a7fa3d9675673e6c88a
BasawarajBiradar <basawarajbiradar007@gmail.com>
2025-08-15 21:52:09 +0530

    code update - retrieve and register API

--- src/main/java/com/example/demo/service/adminService/userManagement/impl/UserManagementServiceImpl.java
+++ src/main/java/com/example/demo/service/adminService/userManagement/impl/UserManagementServiceImpl.java
@@ -37,32 +37,33 @@ public class UserManagementServiceImpl implements UserManagementService {
         user.setEmailId(requestBody.getEmailId());
         user.setFirstName(requestBody.getFirstName());
         user.setLastName(requestBody.getLastName());
+        user.setActiveStatus(true);
         UserMst savedUser = userRepository.save(user);
         return ResponseHandler.success(savedUser, "Success", "200");
     }
 
     @Override
     public ResponseEntity<?> serviceEntryPointForRetrieveUsers(RetrieveUsersRequestBody requestBody) {
-//        List<Object[]> resultList = this.userRepository.retrieveUsersData(requestBody.getUserId());
-//        List<RetrieveUsersResultModel> resultModel = new ArrayList<>();
-//        for (Object[] res : resultList) {
-//            String userName = res[0] == null ? null : res[0].toString();
-//            String password = res[1] == null ? null : res[1].toString();
-//            String role = res[2] == null ? null : res[2].toString();
-//            String email = res[3] == null ? null : res[3].toString();
-//            String createdOn = res[4] == null ? null : res[4].toString();
-//            String createdBy = res[5] == null ? null : res[5].toString();
-//            String lastUpdatedOn = res[6] == null ? null : res[6].toString();
-//            String lastUpdatedBy = res[7] == null ? null : res[7].toString();
-//            String activeStatus = res[8] == null ? null : res[8].toString();
-//
-//            resultModel.add(new RetrieveUsersResultModel(userName, password, role, email,
-//                    createdOn, createdBy, lastUpdatedOn, lastUpdatedBy, activeStatus));
-//        }
-//        resultModel.sort((a,b) -> a.getUserName().compareToIgnoreCase(b.getUserName()));
-//        return ResponseHandler.success(resultModel, "Success", "200");
-        List<UserMst> resultList = this.userRepository.findAll();//(requestBody.getUserId());
-        return ResponseHandler.success(resultList, "Success", "200");
+        List<Object[]> resultList = this.userRepository.retrieveUsersData(requestBody.getUserId());
+        List<RetrieveUsersResultModel> resultModel = new ArrayList<>();
+        for (Object[] res : resultList) {
+            String userName = res[0] == null ? null : res[0].toString();
+            String password = res[1] == null ? null : res[1].toString();
+            String role = res[2] == null ? null : res[2].toString();
+            String email = res[3] == null ? null : res[3].toString();
+            String createdOn = res[4] == null ? null : res[4].toString();
+            String createdBy = res[5] == null ? null : res[5].toString();
+            String lastUpdatedOn = res[6] == null ? null : res[6].toString();
+            String lastUpdatedBy = res[7] == null ? null : res[7].toString();
+            String activeStatus = res[8] == null ? null : res[8].toString();
+
+            resultModel.add(new RetrieveUsersResultModel(userName, password, role, email,
+                    createdOn, createdBy, lastUpdatedOn, lastUpdatedBy, activeStatus));
+        }
+        resultModel.sort((a,b) -> a.getUserName().compareToIgnoreCase(b.getUserName()));
+        return ResponseHandler.success(resultModel, "Success", "200");
+//        List<UserMst> resultList = this.userRepository.findAll();//(requestBody.getUserId());
+//        return ResponseHandler.success(resultList, "Success", "200");
 
     }
 
